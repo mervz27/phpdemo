144f83ae87a8eba13cb6526b25cb32b833599398
Corvinus <markus.corvinus0827@gmail.com>
2024-10-05 13:31:27 +0800

    demo2 done

---
+++ demo1.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP DEMO</title>
+</head>
+<body>
+    <form action="output.php" method="GET">
+        <div>
+            <label for="username">Username</label>
+            <input type="text" name="username" id="username">
+        </div>
+
+        <div>
+            <label for="email">Email</label>
+            <input type="email" name="email" id="email">
+        </div>
+
+        <div>
+            <label for="password1">Password</label>
+            <input type="password" name="password1" id="password1">
+        </div>
+        
+        <div>
+            <label for="confirm-password">Confirm Password</label>
+            <input type="password" name="confirm-password" id="confirm-password">
+        </div>
+
+        <input type="submit" value="Register">
+    </form>
+</body>
+</html>
